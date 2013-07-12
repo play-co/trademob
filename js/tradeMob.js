21e7d92ea75dd918c0599b4b1c8e930075436479
Chris A. Taylor <chris@gameclosure.com>
2013-07-12 15:40:03 -0700

    Fix log

--- js/tradeMob.js
+++ js/tradeMob.js
@@ -1,6 +1,6 @@
 var TradeMob = Class(function () {
 	this.track = function (name, data) {
-		logger.log("{tradeMob} track: ", name, data);
+		logger.log("{tradeMob} track: ", name, JSON.stringify(data));
 		NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
 			NATIVE.plugins.sendEvent("TradeMobPlugin", "track",
 				JSON.stringify({ eventName: name, params: data }));
